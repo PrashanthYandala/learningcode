eab7b66a26b760a8b34275afa676f8794bf4f8f0
Prashanth2927 <54016176+Prashanth2927@users.noreply.github.com>
2020-02-21 12:58:35 +0530

    21.0

--- src/Routing.js/LoginPage.js
+++ src/Routing.js/LoginPage.js
@@ -1,14 +1,42 @@
 import React, { Component } from "react";
+import data from "./data"; 
 
 class LoginPage extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username:"",
+      password:''
+    }
+    this.inputRef= React.createRef();
+  }
+
+  componentDidMount(){
+    this.inputRef.current.focus()
+    console.log(this.inputRef)
+  }
+
+  loginHandler =(e)=>{
+  e.preventDefault();
+   this.setState({
+     username: this.inputRef.current.value,
+     password: this.inputRef.current.value,
+     
+    })
+  }
+
   render() {
+    console.log({username})
+    const credentials=data.users;
+    const {username,password}=this.state;
     return (
       <React.Fragment>
         <div>
-          <form>
+       
+          <form onSubmit={this.loginHandler}>
             <div>
               <label>Username</label>
-              <input type="text" />
+              <input type="text" ref={this.inputRef}/>
             </div>
 
             <div>
@@ -17,7 +45,9 @@ class LoginPage extends Component {
             </div>
 
             <button type="submit">Submit</button>
+          
           </form>
+
         </div>
       </React.Fragment>
     );
